fd2e1b69114e7d9dd6cd90cc87cc045c29b1122d
Venray92 <venray99@gmail.com>
2026-09-16 07:07:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+import os
+import sys
+
+# Mendaftarkan Root Directory ke Python path secara eksplisit
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
+
 import streamlit as st
 
 # Import modul UI Views

560c92fdf9c5eae40762201ef50720d20b2d5a28
Venray92 <venray99@gmail.com>
2026-09-17 09:31:53 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+# Import modul UI Views & Helper
+from utils.ui_helpers import inject_custom_css
+from views.header import render_header, render_welcome
+from views.tab_rsi import render_tab_rsi
+from views.tab_stoch_psar import render_tab_stoch_psar
+from views.tab_trade_planner import render_tab_trade_planner
+
+# 1. Konfigurasi Halaman Streamlit
+st.set_page_config(
+    page_title="Z-QUANT",
+    page_icon="⚡",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
+
+# 2. Inject Custom CSS
+inject_custom_css()
+
+# 3. Inisialisasi Session State
+if "selected_screener" not in st.session_state:
+    st.session_state["selected_screener"] = None
+
+# 4. Render Header UI & Navigation Menu
+render_header()
+
+# 5. Routing Halaman Berdasarkan Screener Terpilih
+screener = st.session_state["selected_screener"]
+
+if screener is None:
+    render_welcome()
+elif screener == "rsi":
+    render_tab_rsi()
+elif screener == "stoch_psar":
+    render_tab_stoch_psar()
+elif screener == "trade_plan":
+    render_tab_trade_planner()

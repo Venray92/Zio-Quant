3633aad534f00ed21c7643c08ec514a7037149a6
Venray92 <venray99@gmail.com>
2026-09-17 10:10:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 # Import modul UI Views & Helper
 from utils.ui_helpers import inject_custom_css
-from views.header import render_header, render_welcome
+from views.header import render_header, render_header_divider, render_welcome
+from views.top_nav import render_top_nav
 from views.tab_rsi import render_tab_rsi
 from views.tab_stoch_psar import render_tab_stoch_psar
 from views.tab_trade_planner import render_tab_trade_planner
@@ -22,8 +23,10 @@ inject_custom_css()
 if "selected_screener" not in st.session_state:
     st.session_state["selected_screener"] = None
 
-# 4. Render Header UI & Navigation Menu
+# 4. Render Layout Atas (Header -> Top Nav -> Divider)
 render_header()
+render_top_nav()
+render_header_divider()
 
 # 5. Routing Halaman Berdasarkan Screener Terpilih
 screener = st.session_state["selected_screener"]

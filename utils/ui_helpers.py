d58c38f18eee65bc84977cd5e2e0537785ddac6e
Venray92 <venray99@gmail.com>
2026-09-17 10:14:24 +0700

    Update ui_helpers.py

--- utils/ui_helpers.py
+++ utils/ui_helpers.py
@@ -65,7 +65,7 @@ def inject_custom_css():
         border-radius: 8px !important;
         padding: 6px 16px !important;
         height: 48px !important;
-        width: 100% !important;
+        
         margin-top: 4px !important;
         margin-bottom: 4px !important;
         box-shadow: 0 0 15px #00FF66, inset 0 0 8px rgba(0, 255, 102, 0.4) !important;
